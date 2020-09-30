49c9afd95761b6a5ac6e5bf677a817c7b3211816
Per Unneberg <per.unneberg@scilifelab.se>
2020-09-30 23:28:41 +0200

    Skip genecovr test due to issue with package root

--- tests/testthat/test-genecovr.R
+++ tests/testthat/test-genecovr.R
@@ -1,4 +1,11 @@
-pkg_path <- dirname(file.path(system.file(package = "genecovr")))
+skip("Skip until resolve issue of testing package binary")
+pkg_path <- getwd()
+tmp <- tempdir(check = TRUE)
+withr::local_dir(tmp)
+withr::local_temp_libpaths()
+withr::local_path(file.path(.libPaths()[1], "genecovr", "bin"))
+devtools::install(pkg=pkg_path, quick = TRUE, upgrade = "never")
+
 create_file <- function(fn) system.file("extdata", fn, package = "genecovr")
 
 data <- as.data.frame(rbind(
@@ -8,20 +15,14 @@ data <- as.data.frame(rbind(
       create_file("polished.fai"), create_file("transcripts.fai"))
 ))
 colnames(data) <- NULL
-
-tmp <- tempdir(check = TRUE)
-withr::local_dir(tmp)
 withr::local_file(write.csv(data, file = "assemblies.csv", row.names = FALSE))
-withr::local_temp_libpaths()
-devtools::install(pkg_path, quick = TRUE, upgrade = "never")
-withr::local_path(file.path(.libPaths()[1], "genecovr", "bin"))
 
 test_that("genecovr runs as expected", {
     system(paste(paste0("R_LIBS_USER=", .libPaths()[1]),
                  "genecovr", "assemblies.csv"))
     expect_equal(length(list.files(pattern = "*.png")), 13)
     expect_equal(length(grep("Rplots", list.files(pattern = "*.pdf"),
-                             invert = TRUE), 13))
+                             invert = TRUE)), 13)
     expect_equal(length(list.files(pattern = "*.csv.gz")), 4)
 })
 
