ec14d154dce72431e2f626ba5e0e02ac2910d137
Per Unneberg <per.unneberg@scilifelab.se>
2023-12-18 12:43:59 +0100

    Update tests and documentation for 0.1.0 release

--- tests/testthat/test-genecovr.R
+++ tests/testthat/test-genecovr.R
@@ -1,29 +1,36 @@
-skip("Skip until resolve issue of testing package binary")
 pkg_path <- getwd()
 tmp <- tempdir(check = TRUE)
 withr::local_dir(tmp)
 withr::local_temp_libpaths()
 withr::local_path(file.path(.libPaths()[1], "genecovr", "bin"))
-devtools::install(pkg=pkg_path, quick = TRUE, upgrade = "never")
+devtools::install(pkg = pkg_path, quick = TRUE, upgrade = "never")
 
 create_file <- function(fn) system.file("extdata", fn, package = "genecovr")
 
 data <- as.data.frame(rbind(
-    c("nonpol", create_file("transcripts2nonpolished.psl"),
-      create_file("nonpolished.fai"), create_file("transcripts.fai")),
-    c("pol", create_file("transcripts2polished.psl"),
-      create_file("polished.fai"), create_file("transcripts.fai"))
+  c(
+    "nonpol", create_file("transcripts2nonpolished.psl"),
+    create_file("nonpolished.fai"), create_file("transcripts.fai")
+  ),
+  c(
+    "pol", create_file("transcripts2polished.psl"),
+    create_file("polished.fai"), create_file("transcripts.fai")
+  )
 ))
 colnames(data) <- NULL
 withr::local_file(write.csv(data, file = "assemblies.csv", row.names = FALSE))
 
 test_that("genecovr runs as expected", {
-    system(paste(paste0("R_LIBS_USER=", .libPaths()[1]),
-                 "genecovr", "assemblies.csv"))
-    expect_equal(length(list.files(pattern = "*.png")), 13)
-    expect_equal(length(grep("Rplots", list.files(pattern = "*.pdf"),
-                             invert = TRUE)), 13)
-    expect_equal(length(list.files(pattern = "*.csv.gz")), 4)
+  system(paste(
+    paste0("R_LIBS_USER=", .libPaths()[1]),
+    "genecovr", "assemblies.csv"
+  ))
+  expect_equal(length(list.files(pattern = "*.png")), 15)
+  expect_equal(length(grep("Rplots", list.files(pattern = "*.pdf"),
+    invert = TRUE
+  )), 15)
+  expect_equal(length(list.files(pattern = "*.csv.gz")), 4)
+  expect_equal(length(list.files(pattern = "*.tsv.gz")), 1)
 })
 
-withr::defer(unlink(dir, recursive = TRUE))
+withr::defer(unlink(tmp, recursive = TRUE))
